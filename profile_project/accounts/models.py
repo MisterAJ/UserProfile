f677d61eae6d3e097b58fd53bd6d6110498aec3f
MisterAJ <ajlongstreet@gmail.com>
2018-01-06 14:18:09 -0500

    adds ability to edit password and profile

--- profile_project/accounts/models.py
+++ profile_project/accounts/models.py
@@ -1,11 +1,22 @@
 import datetime
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 class Account(models.Model):
-    first_name = models.CharField(max_length=75)
-    last_name = models.CharField(max_length=75)
-    email = models.EmailField(max_length=75)
-    date_of_birth = models.DateField(default=datetime.date.today)
-    bio = models.TextField(max_length=512)
-    avatar = models.ImageField(default='No Image')
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True
+    )
+    first_name = models.CharField(max_length=75, blank=True)
+    last_name = models.CharField(max_length=75, blank=True)
+    email = models.EmailField(max_length=75, blank=True)
+    date_of_birth = models.DateField(default=datetime.date.today, blank=True)
+    bio = models.TextField(max_length=512, blank=True)
+    avatar = models.ImageField(default='No Image', blank=True)
+
+    def __str__(self):
+        if self.email:
+            return self.email
+        return self.first_name
--- profile_project/accounts/views.py
+++ profile_project/accounts/views.py
@@ -1,11 +1,14 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import (AuthenticationForm, UserCreationForm,
+                                        PasswordChangeForm)
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from . import forms
+from .forms import AccountForm
+from .models import Account
 
 
 def sign_in(request):
@@ -18,7 +21,7 @@ def sign_in(request):
                 if user.is_active:
                     login(request, user)
                     return HttpResponseRedirect(
-                        reverse('home')  # TODO: go to profile
+                        reverse('accounts:profile')
                     )
                 else:
                     messages.error(
@@ -34,25 +37,98 @@ def sign_in(request):
 
 
 def sign_up(request):
-    form = forms.AccountForm()
+    form = UserCreationForm()
     if request.method == 'POST':
-        form = forms.AccountForm(data=request.POST)
+        form = UserCreationForm(data=request.POST)
         if form.is_valid():
+            username = form.cleaned_data['username']
             form.save()
             user = authenticate(
-                username=form.cleaned_data['username'],
+                username=username,
                 password=form.cleaned_data['password1']
             )
+            account_form = AccountForm()
+            account_form.first_name = username
+            account_form.user = user
+            if account_form.is_valid():
+                account_form.save()
             login(request, user)
             messages.success(
                 request,
                 "You're now a user! You've been signed in, too."
             )
-            return HttpResponseRedirect(reverse('home'))  # TODO: go to profile
+            return HttpResponseRedirect(reverse('accounts:profile'))
     return render(request, 'accounts/sign_up.html', {'form': form})
 
-
+@login_required
 def sign_out(request):
     logout(request)
     messages.success(request, "You've been signed out. Come back soon!")
     return HttpResponseRedirect(reverse('home'))
+
+@login_required
+def profile(request):
+    form = AccountForm()
+    if request.method == 'POST':
+        form = AccountForm(data=request.POST)
+        if form.is_valid():
+            if request.user.is_authenticated():
+                form.save()
+                return HttpResponseRedirect(
+                    reverse('accounts:profile')
+                )
+            else:
+                messages.error(
+                    request,
+                    "That user account has been disabled."
+                )
+        else:
+            messages.error(
+                request,
+                "Form input is invalid."
+            )
+    return render(request, 'accounts/profile.html', {'form': form})
+
+@login_required
+def edit_profile(request, account_pk):
+    account = get_object_or_404(Account, pk=account_pk)
+    form = AccountForm(instance=account)
+    if request.method == 'POST':
+        form = AccountForm(instance=account, data=request.POST)
+        if form.is_valid():
+            if request.user.is_authenticated():
+                form.user = request.user
+                form.save()
+                return HttpResponseRedirect(
+                    reverse('accounts:bio')
+                )
+            else:
+                messages.error(
+                    request,
+                    "That user account has been disabled."
+                )
+        else:
+            messages.error(
+                request,
+                "Form input is invalid."
+            )
+    return render(request, 'accounts/profile.html', {'form': form})
+
+def bio(request):
+    '''User Bio Page'''
+    return render(request, 'accounts/bio.html')
+
+def change_password(request):
+    form = PasswordChangeForm(request.user)
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, data=request.POST)
+        if form.is_valid():
+            old = form.cleaned_data['old_password']
+            new = form.cleaned_data['new_password1']
+            if old == new:
+                messages.error(request, 'You can not use the same password.')
+            else:
+                form.save()
+                messages.success(request, 'You have updated your password.')
+                return HttpResponseRedirect(reverse('accounts:bio'))
+    return render(request, 'accounts/password.html', {'form': form})
